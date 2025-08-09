fac500fdf94c0dfdede88fe679c6a63d67f5fd6a
François LaBerge <fgrclaberge@gmail.com>
2025-08-09 12:38:37 -0400

    use config file

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 import { meet } from '@googleworkspace/meet-addons/meet.addons';
 import config from './config';
 
-const CLOUD_PROJECT_NUMBER = 'CLOUD_PROJECT_NUMBER';
-const MAIN_STAGE_URL = 'MAIN_STAGE_URL';
-
 export async function setUpAddon() {
 	const session = await meet.addon.createAddonSession({
-		cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+		cloudProjectNumber: config.CLOUD_PROJECT_NUMBER,
 	});
 	const sidePanelClient = await session.createSidePanelClient();
 	document
 		.getElementById('start-activity')
 		.addEventListener('click', async () => {
 			await sidePanelClient.startActivity({
-				mainStageUrl: MAIN_STAGE_URL
+				mainStageUrl: config.MAIN_STAGE_URL
 			});
 		});
 }
